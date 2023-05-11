c05d7796f09ea5c6544eb618d2d92091903158ec
abr-ya <gamer05@list.ru>
2023-05-11 14:01:06 +0300

    feature: Add simple form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
+import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
 import "./App.css";
 
-const App = () => (
-  <div className="container">
-    <h1>Hello, Vite + TypeScript + React + ESLint + Prettier!</h1>
-  </div>
-);
+interface IForm {
+  age: number;
+  name: string;
+}
+
+const App = () => {
+  const defaultValues = {
+    age: 22,
+  };
+
+  const { register, handleSubmit } = useForm<IForm>({ defaultValues });
+
+  const submitHandler: SubmitHandler<IForm> = (data) => {
+    console.log("data", data);
+  };
+
+  const errorHandler: SubmitErrorHandler<IForm> = (errors) => {
+    console.log("errors", errors);
+  };
+
+  const validateName = (name: string) => {
+    console.log("validate name:", name);
+
+    return true;
+  };
+
+  return (
+    <div className="container">
+      <h1>Hello, Vite + TypeScript + React + ESLint + Prettier!</h1>
+      <form onSubmit={handleSubmit(submitHandler, errorHandler)}>
+        <input type="text" {...register("name", { required: true, validate: validateName })} />
+        <input type="number" {...register("age")} />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
