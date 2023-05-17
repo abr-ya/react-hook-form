4b1a487aeb92da65edb05dde6c5c200fb8265057
abr-ya <gamer05@list.ru>
2023-05-17 21:04:41 +0300

    feature: Add React Select async demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
+import axios from "axios";
+import AsyncSelect from "react-select/async";
+import { StateOption, stateOptions } from "./data";
 import "./App.css";
-import { useEffect } from "react";
 
 interface IForm {
   age: number;
@@ -41,6 +43,26 @@ const App = () => {
     return true;
   };
 
+  const filterColors = (inputValue: string) => {
+    return stateOptions.filter((i) => i.label.toLowerCase().includes(inputValue.toLowerCase()));
+  };
+
+  const promiseOptions = (inputValue: string) =>
+    new Promise<StateOption[]>((resolve) => {
+      setTimeout(() => {
+        resolve(filterColors(inputValue));
+      }, 1000);
+    });
+
+  const getCountries = (inputValue: string) => {
+    return axios.get(`https://restcountries.com/v3.1/name/${inputValue}`).then((res) => {
+      console.log(res);
+      const options = res.data.map((el: any) => ({ label: el.translations.rus.common, value: el.name.common }));
+      console.log(options);
+      return options;
+    });
+  };
+
   return (
     <div className="container">
       <h1>Hello, Vite + TypeScript + React + ESLint + Prettier!</h1>
@@ -55,7 +77,20 @@ const App = () => {
           Set Name
         </button>
       </form>
-      <div>Текущее имя: {watch("name")}</div>
+      <div>Current name: {watch("name")}</div>
+
+      <h2>AsyncSelect demo</h2>
+      <h3>States (timeout)</h3>
+      <AsyncSelect isMulti cacheOptions defaultOptions loadOptions={promiseOptions} />
+
+      <h3>Countries (axios)</h3>
+      <AsyncSelect
+        isMulti
+        cacheOptions
+        loadOptions={getCountries}
+        placeholder="Start typing"
+        noOptionsMessage={({ inputValue }) => (inputValue ? "No opt 1" : "No opt 0")}
+      />
     </div>
   );
 };
